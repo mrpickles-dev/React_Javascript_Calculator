3a590bb5c2921c2672f108bc5c4e17deebe0d555
mrpickles-dev <mr.pickles300@gmail.com>
2021-04-21 13:38:44 +0200

    finished the logical part

--- script.js
+++ script.js
@@ -1,117 +1,116 @@
-
-
-const number = [0, 1 , 2, 3, 4, 5, 6, 7, 8, 9];
-const operation = ['=', '+','-','/','*','AC', '.'];
-
+const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+const ops = ['/', '*', '-', '+']
+const ids = {
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  0: 'zero',
+  '/': 'divide',
+  '*': 'multiply',
+  '-': 'subtract',
+  '+': 'add'
+}
 
 
 class App extends React.Component {
     
 state = {
-
-    lastPressed:undefined,
-    currentNumber: '0',
-    preResult: undefined,
-    operator: undefined
-
+    lastPressed: undefined,
+    preResult: '0',
+    operation: undefined
 }
 
-
-    handleClcik = (e) => {
-       const {lastPressed, currentNumber, preResult, operator} = this.state;
-        const {innerText} = e.target;
-
+          
+handleClick = e => {
+    const { preResult, lastPressed } = this.state
+    const { innerText } = e.target
+       
+    console.log(innerText)
+    
+    switch(innerText) {
+      case 'AC': {
         this.setState({
-            lastPressed: innerText
+          preResult: '0'
         })
-
-        if(!Number.isNaN(Number(innerText))){
-            if (currentNumber === '0'){
-                this.setState({
-                    currentNumber: innerText,
-                   
-                }  );
-            }else {
-            this.setState({
-                currentNumber: currentNumber + innerText,
-                
-            })
-        } 
-        return ;
-        }
-
-       switch(innerText){    
-        case 'AC': {
-
-            this.setState({
-
-                currentNumber: '0',
-                preResult: undefined,
-                operator: undefined
-            })
-            break
-        }
-        case '.': {
-            if (!currentNumber.includes('.')){
-                this.setState({
-                    currentNumber: currentNumber + innerText
-                })
-                
-            }break
-        }
-        default: {
-
-            if(!operator){
-                this.setState({
-                    operator: innerText,
-                    preResult: currentNumber,
-                    currentNumber: ''
-
-                });
-            } else if (innerText === '='){
-                const total = eval(`${preResult} ${operator} ${currentNumber}`);
-                this.setState({
-                    operator: undefined,
-                    preResult: total,
-                    currentNumber: total
-                })
-
-                } else {
-                    const total = eval(`${preResult} ${operator} ${currentNumber}`);
-
-                    this.setState({
-                        operator: innerText,
-                        preResult: total,
-                        currentNumber: total
-                    })
-                }
-            
-        }    
-
+        break
       }
-  
-
+      case '=': {
+        const evaluated = eval(preResult)
+        this.setState({
+          preResult: evaluated
+        })
+        break
+      }
+      case '.': {
+        const splitted = preResult.split(/[\+\-\*\/]/)
+        const last = splitted.slice(-1)[0]
+        
+        if(!last.includes('.')) {
+          this.setState({
+            preResult: preResult+'.'
+          })
+        }
+        break
+      }
+        
+      default: {
+        let e = undefined
+        //if input is included in operator 
+        if(ops.includes(innerText)) {
+            // only if input is -
+          if(ops.includes(lastPressed) && innerText !== '-') {
+            const lastNumberIdx = preResult.split('').reverse().findIndex(char => char !== ' ' && nums.includes(+char))
+            e = preResult.slice(0, preResult.length - lastNumberIdx) + ` ${innerText} `
+          } else {
+            e = `${preResult} ${innerText} `
+          }
+        } else {
+            e = (preResult === '0') ? innerText : (preResult + innerText)
+        }
+        
+        this.setState({
+          preResult: e,
+        })
+      }
     }
+    
+    this.setState({
+      lastPressed: innerText
+    })
+  }
+   
 
     render(){
-        const {currentNumber, preResult, operator} = this.state;
+        const {preResult} = this.state;
     return ( 
+   
     <div className="container">
-        <p style={{position: 'absolute', top:0}}>{JSON.stringify(this.state, null, 2)}</p>
-     <div className="Display">
-         <div className="miniDisplay">{preResult} {operator}</div>
-         {currentNumber}</div>   
+        
+      <div className="Display" id="display">
+        {preResult} 
+        </div>   
+        <div className="keyboard">
      <div className="number">
-     {number.map(numb => (
-         <button className="numbers" key={numb} onClick={this.handleClcik}>{numb} </button>
+     <button className="btn btn-primary ac" onClick={this.handleClick} id="clear">AC</button>
+     {nums.map(numb => (
+         <button className={`dark-grey ${numb === 0 && 'big-h'}`} key={numb} onClick={this.handleClick} id={ids[numb]}>{numb} </button>
      ) ) }
+     <button className="btn btn-success" onClick={this.handleClick} id="equals">=</button>
      </div>
      <div className="inputButton">
-     {operation.map(op => (
-         <button className="operation" key={op} onClick={this.handleClcik}>{op} </button>
+     <button className="btn btn-info decimal" onClick={this.handleClick} id="decimal">.</button>
+     {ops.map(op => (
+         <button className="operation" key={op} onClick={this.handleClick} id={ids[op]}> {op} </button>
      ) ) }
+     
     </div>
-
+</div>
    </div>
 )
 }
